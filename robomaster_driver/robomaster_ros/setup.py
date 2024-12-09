8ae852b719f44bf2a85967a5f8cec2a4bda93d9e
zhongweiheng <zhongweiheng@megvii.com>
2024-12-09 17:13:48 +0800

    fix: colcon build error

--- robomaster_driver/robomaster_ros/setup.py
+++ robomaster_driver/robomaster_ros/setup.py
@@ -6,9 +6,7 @@ package_name = 'robomaster_ros'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name,
-              package_name + '.modules'
-              ],
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
